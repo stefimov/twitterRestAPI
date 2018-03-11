356ab781cb9862174d5d82bc2deda59708463caa
STAS <STAS@STAS-PC>
2018-03-11 23:55:04 +0300

    adding a class to store authorization data

---
+++ restAssuredTwitter/src/test/java/restAssuredTwitter/AuthorizationDataStorage.java
@@ -0,0 +1,17 @@
+package restAssuredTwitter;
+
+public class AuthorizationDataStorage {
+	//Имя, ключ и секретный код первого тестового аккаунта
+	public final String consumer_1_Name = "Dum_muD";
+	public final String consumer_1_Key = "JTRQYPlsNdDKGpnzzPS8Kwe9u";
+	public final String consumer_1_Secret = "gHJc4h1JVvlDK4VUpSJHo15lyze12I5AOPPhyjZcXdvXu5w3ID";
+	
+	//Имя, ключ и секретный код первого тестового аккаунта
+	public final String consumer_2_Name = "";
+	public final String consumer_2_Key = "";
+	public final String consumer_2_Secret = "";
+	
+	//Токены twitter-приложения через которое осуществляется работа с Twitter API
+	public final String application_Token = "972147370938064896-DhCZwr0tBCcrtd8I5QcFQARaV4Hjjl7";
+	public final String application_Secret = "CzZaMBGSReGKy9IMAKPxvU1z7dov6dYbDokqSc9oS0E72";
+}
