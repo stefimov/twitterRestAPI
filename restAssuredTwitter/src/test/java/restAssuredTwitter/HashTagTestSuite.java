4b93b6a0c6653d399d1fc37f66833c15272916cd
STAS <STAS@STAS-PC>
2018-03-12 01:33:44 +0300

    adding/configuring new test suite for testing hashtag posting

---
+++ restAssuredTwitter/src/test/java/restAssuredTwitter/HashTagTestSuite.java
@@ -0,0 +1,39 @@
+package restAssuredTwitter;
+
+import static io.restassured.RestAssured.basePath;
+import static io.restassured.RestAssured.baseURI;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+class HashTagTestSuite {
+	//Добавляем переменную класса AuthorizationDataStorage чтоб иметь доступ к данным, необходимым для авторизации.
+	static final AuthorizationDataStorage authData = new AuthorizationDataStorage();
+		
+	/*
+	 * Начальные установки для набора тестов
+	 * */
+	@BeforeAll
+	static void setUp()
+	{
+		//устанавливаем стандартный путь для запросов на взаимодействие с твитами
+		baseURI = "https://api.twitter.com";
+		basePath = "/1.1/statuses";
+	}
+		
+	/*
+	 * Функция выполняемая после каждого тест-кейса.
+	 * Необходима для удаления созданных в тест-кейсе твитов.
+	 * */
+	@AfterEach
+	void clearTweets()
+	{
+		GarbageTweetsHandler.clearGarbage();
+	}
+	//@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
