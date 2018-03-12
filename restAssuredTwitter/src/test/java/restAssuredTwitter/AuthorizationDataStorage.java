0b64b647030643bc8bd9931518e4b945238ab8c3
STAS <STAS@STAS-PC>
2018-03-12 04:45:08 +0300

    2nd account data

--- restAssuredTwitter/src/test/java/restAssuredTwitter/AuthorizationDataStorage.java
+++ restAssuredTwitter/src/test/java/restAssuredTwitter/AuthorizationDataStorage.java
@@ -2,16 +2,19 @@ package restAssuredTwitter;
 
 public class AuthorizationDataStorage {
 	//Имя, ключ и секретный код первого тестового аккаунта
-	public final String consumer_1_Name = "Dum_muD";
-	public final String consumer_1_Key = "JTRQYPlsNdDKGpnzzPS8Kwe9u";
-	public final String consumer_1_Secret = "gHJc4h1JVvlDK4VUpSJHo15lyze12I5AOPPhyjZcXdvXu5w3ID";
+	public final String consumer_2_Name = "Dum_muD";
+	public final String consumer_2_Key = "JTRQYPlsNdDKGpnzzPS8Kwe9u";
+	public final String consumer_2_Secret = "gHJc4h1JVvlDK4VUpSJHo15lyze12I5AOPPhyjZcXdvXu5w3ID";
 	
 	//Имя, ключ и секретный код первого тестового аккаунта
-	public final String consumer_2_Name = "";
-	public final String consumer_2_Key = "";
-	public final String consumer_2_Secret = "";
+	public final String consumer_1_Name = "Dum_muD1";
+	public final String consumer_1_Key = "8hbTXXldjDQ8R4l2KMwFFCTYA";
+	public final String consumer_1_Secret = "6d2FFCmIY1JIW7znU5qlx8q98M73hNYwmhga98cIYPLZczKvsL";
 	
 	//Токены twitter-приложения через которое осуществляется работа с Twitter API
-	public final String application_Token = "972147370938064896-DhCZwr0tBCcrtd8I5QcFQARaV4Hjjl7";
-	public final String application_Secret = "CzZaMBGSReGKy9IMAKPxvU1z7dov6dYbDokqSc9oS0E72";
+	/*public final String application_Token = "972147370938064896-DhCZwr0tBCcrtd8I5QcFQARaV4Hjjl7";
+	public final String application_Secret = "CzZaMBGSReGKy9IMAKPxvU1z7dov6dYbDokqSc9oS0E72";*/
+	
+	public final String application_Token = "972978433641861120-o3lpfjVrvSjyqAxnJraV5fcIEahSjPI";
+	public final String application_Secret = "EqSAvvUfVdsPInU8kSJ3FSJg6bPOf4pRFjmuW4vuXlKNN";
 }
